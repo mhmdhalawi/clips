4f9ad21e4c82167b5f75f25d64e36ad3ff8f65dc
mhmdhalawi <mohammadhalawi93@gmail.com>
2022-04-28 19:27:48 +0300

    add auth modal

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+interface IModal {
+  id: string;
+  visible: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalService {
+  private modals: IModal[] = [];
+
+  constructor() {}
+
+  isModalOpen(id: string): boolean {
+    // !! converts to boolean
+    return !!this.modals.find((modal) => modal.id === id)?.visible;
+  }
+
+  toggleModal(id: string): void {
+    const modal = this.modals.find((modal) => modal.id === id);
+    if (modal) {
+      modal.visible = !modal.visible;
+    }
+  }
+
+  register(id: string): void {
+    this.modals.push({
+      id,
+      visible: false,
+    });
+  }
+}
---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+})
+export class ModalComponent implements OnInit {
+  @Input() modalID = '';
+
+  constructor(public modal: ModalService) {}
+
+  ngOnInit(): void {}
+
+  closeModal(): void {
+    this.modal.toggleModal(this.modalID);
+  }
+}
