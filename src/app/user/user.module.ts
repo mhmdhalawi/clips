ad9c9f3ef3de5ba9d5a855ef40cc63a10a072176
mhmdhalawi <mohammadhalawi93@gmail.com>
2022-04-30 18:21:59 +0300

    finish login form validation

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,11 +5,11 @@ import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AuthModalComponent, LoginComponent, RegisterComponent],
-  imports: [CommonModule, SharedModule, ReactiveFormsModule],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule, FormsModule],
   exports: [AuthModalComponent],
 })
 export class UserModule {}
