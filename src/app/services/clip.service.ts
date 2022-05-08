5df456cf4c7ceae20fdedd4a48d297339e25fc0f
mhmdhalawi <mohammadhalawi93@gmail.com>
2022-05-08 18:13:10 +0300

    render clips submitted by the user

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -4,8 +4,9 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
   DocumentReference,
+  QuerySnapshot,
 } from '@angular/fire/compat/firestore';
-import { of, switchMap } from 'rxjs';
+import { of, switchMap, map } from 'rxjs';
 
 import { IClip } from '../models/clip.model';
 
@@ -30,6 +31,9 @@ export class ClipService {
         const query = this.clipsCollection.ref.where('uid', '==', user.uid);
 
         return query.get();
+      }),
+      map((snapShot) => {
+        return (snapShot as QuerySnapshot<IClip>).docs;
       })
     );
   }
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ClipService } from 'src/app/services/clip.service';
+import { IClip } from 'src/app/models/clip.model';
 
 @Component({
   selector: 'app-manage',
@@ -9,6 +10,8 @@ import { ClipService } from 'src/app/services/clip.service';
 })
 export class ManageComponent implements OnInit {
   videoOrder = '1';
+
+  clips: IClip[] = [];
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -22,7 +25,15 @@ export class ManageComponent implements OnInit {
       this.videoOrder = sort === '2' ? sort : '1';
     });
 
-    this.clipService.getUserClips().subscribe(console.log);
+    this.clipService.getUserClips().subscribe((docs) => {
+      this.clips = [];
+      docs.forEach((doc) => {
+        this.clips.push({
+          docID: doc.id,
+          ...doc.data(),
+        });
+      });
+    });
   }
 
   sort(event: Event): void {
